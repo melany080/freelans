7e404a618e115492133a9bac652a98dad30c3d9d
MateoA <mateo.arbelaez.p@gmail.com>
2025-11-24 15:08:03 -0500

    se hace cambio

---
+++ controlador.js
@@ -0,0 +1,20 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+
+    let cajaUsuario=document.getElementById("cajausuario")
+    let cajaCorreo=document.getElementById("cajacorreo")
+    let cajaCiudad=document.getElementById("cajaciudad")
+    let cajaTelefono=document.getElementById("cajatelefono")
+    let cajaContraseña=document.getElementById("cajacontraseña")
+    
+    let datosDelFormulario=(
+        usuario:cajaUsuario.value,
+        cajaCorreo:cajaCorreo.value,
+        ciudad:cajaCiudad.value,
+        telefono:cajaTelefono,
+        contraseña:cajaContraseña,
+
+    )
+
+})
